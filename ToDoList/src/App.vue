b09d6e50cfbac6c9eba0d885f4438c25f80dd62b
yasaman <yasamanalz00@gmail.com>
2026-06-20 10:30:24 +0330

    implement progress bar UI with flex layout

--- ToDoList/src/App.vue
+++ ToDoList/src/App.vue
@@ -1,9 +1,13 @@
 <template>
+  <div>
   <Header/>
+  <ProgressBar/>
+  </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
+import ProgressBar from './components/ProgressBar.vue';
 
 
 </script>
---
+++ ToDoList/src/components/ProgressBar.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="px-3 gap-4 flex mt-7 w-full max-w-2xl items-center mx-auto">
+  <div class="h-1 w-full rounded-3xl bg-gray-100"></div>
+  <p class="text-gray-300"></p>
+</div>
+  </div>
+</template>
