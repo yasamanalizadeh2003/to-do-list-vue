9f91f605d351863ce31886b42587b79aa5cc5b6b
yasaman <yasamanalz00@gmail.com>
2026-06-20 21:51:17 +0330

    implement progress calculation and progress bar

--- ToDoList/src/components/ProgressBar.vue
+++ ToDoList/src/components/ProgressBar.vue
@@ -1,9 +1,22 @@
-<script setup></script>
+<script setup>
+function progress(completed, total) {
+  if (total === 0) {
+    return { width: '0%' }
+  }
+
+  return {
+    width: `${(completed / total) * 100}%`
+  }
+}
+</script>
 
 <template>
   <div>
-   <div class="px-3 sm:px-6 flex mt-7 w-full max-w-2xl items-center mx-auto gap-4">
-  <div class="h-1 w-full rounded-3xl bg-gray-100"></div>
+   <div class="px-3 sm:px-6 flex mt-7 w-full max-w-2xl  items-center mx-auto gap-4">
+  <div class="h-1 w-full rounded-3xl bg-gray-200">
+  <div class="bg-gradient-to-r from-pink-100 via-pink-300 to-pink-500
+   transition-all duration-500 rounded-3xl h-1" :style="progress(1,4)"></div>
+  </div>
   <p class="text-gray-300 text-sm sm:text-base"></p>
 </div>
   </div>
