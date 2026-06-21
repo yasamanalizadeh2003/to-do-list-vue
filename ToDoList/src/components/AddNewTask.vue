1b4c2335356f649e149dd1453af4f225f195dd6e
yasaman <yasamanalz00@gmail.com>
2026-06-21 11:07:16 +0330

    feat: implement tag selection tabs with active state

--- ToDoList/src/components/AddNewTask.vue
+++ ToDoList/src/components/AddNewTask.vue
@@ -1,17 +1,35 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { Plus } from 'lucide-vue-next'
+import { onClickOutside } from '@vueuse/core'
 
 const tag = ref('')
 const tags = ['work', 'personal', 'urgent']
+const tabController=ref()
+const controller=ref(false)
+const card = ref(null)
+
+function tabs(data,index){
+tag.value=data
+console.log(tag.value)
+tabController.value=index
+
+}
+
+onClickOutside(card, () => {
+  controller.value = false
+})
 </script>
 
 <template>
   <div>
-    <div class="flex px-5 w-full max-w-2xl mx-auto">
-      <div
+    <div class="flex px-5 w-full max-w-2xl mx-auto max-h-min">
+      <div 
+       ref="card"
+      @click="controller=true"
         tabindex="0"
-        class="border-2 border-gray-200 shadow-sm h-20 focus:border-pink-300 shadow-gray-300 mt-14 w-full max-w-2xl mx-auto outline-0 focus:shadow-sm focus:shadow-pink-300 rounded-sm p-6"
+        class="border-2 border-gray-200 shadow-sm  focus-within:border-pink-300
+         shadow-gray-300 mt-14 w-full max-w-2xl mx-auto outline-0 focus-within:shadow-sm focus-within:shadow-pink-300 rounded-sm p-6"
       >
         <div class="flex justify-between items-center">
           <div class="flex gap-3 items-center">
@@ -23,7 +41,49 @@ const tags = ['work', 'personal', 'urgent']
             <Plus class="w-5 h-5 text-white" />
           </div>
         </div>
+
+      <Transition name="expand">
+  <div v-if="controller" class="w-full">
+    <hr class="text-gray-100 my-4 w-full" />
+
+    <div class="flex gap-3">
+      <div class="text-gray-300 font-light font-mono text-sm self-center">
+        TAG
+      </div>
+
+      <div
+        v-for="(t,index) in tags"
+        :key="index"
+        @click="tabs(t,index)"
+        :class="tabController!=index?'text-gray-500 font-light font-mono text-sm bg-gray-100 px-2 py-1':
+        'text-white font-light font-mono text-sm bg-pink-300 px-2 py-1'"
+      >
+        {{ t }}
+      </div>
+    </div>
+  </div>
+</Transition>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.expand-enter-active,
+.expand-leave-active {
+  transition: all 0.3s ease;
+  overflow: hidden;
+}
+
+.expand-enter-from,
+.expand-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.expand-enter-to,
+.expand-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
